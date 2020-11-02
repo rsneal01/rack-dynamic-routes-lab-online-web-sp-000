05801d9ade4a40ed63c5d2b8fcfe774f82d9a80b
Bobby Neal <rsneal01@gmail.com>
2020-11-02 17:38:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     
     
     if req.path.match(/items/)
-      item_name = req.path.split(/items/).last
+      item_name = req.path.split("/items/").last
     else
       resp.write "Route not found"
       resp.status = 404
