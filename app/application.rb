f4b09cb479f7256b2038d14285c3c77438bc733b
Bobby Neal <rsneal01@gmail.com>
2020-11-02 17:37:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     req = Rack::Request.new(env)
     
     
-    if req.path.match("/items/")
+    if req.path.match(/items/)
       resp.write item_name.price
     else
       resp.write "Route not found"
